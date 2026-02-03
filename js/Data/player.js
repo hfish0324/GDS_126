886c5b34840a6579ed5569d13bb1c0539f7ed428
Harrison <hsfisher@email.neit.edu>
2026-02-03 12:18:25 -0500

    Update player.js

--- js/Data/player.js
+++ js/Data/player.js
@@ -19,18 +19,20 @@ Properties:
 
 var playerData ={
 	info:{
-		src:`images/snale.png`
+		src:`images/Smiley.png`
 	},
 	states:{
 		//The idle animation 
     	idle:
 		{
-			fps:15,
+			fps:30,
 			cycle:true,
 			frames:
 			[
 				{width:128, height:128, startX:0, startY:0},
-				{width:128, height:128, startX:128, startY:0}
+				{width:128, height:128, startX:128, startY:0},
+				{width:128, height:128, startX:256, startY:0},
+				{width:128, height:128, startX:384, startY:0}
 				
 			]
 		},
